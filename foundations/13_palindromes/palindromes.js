2fb91848a1a235ddef2a6968c9b489043ae874ac
Spxchaos <amannaiknov@gmail.com>
2026-05-06 11:24:44 +0200

    done with palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+   const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const oldString = string.toLowerCase()
+                            .split('')
+                            .filter((character) => alphanumerical.includes(character))
+                            .join('');
+
+
+    const newString = oldString.split('')
+                               .reverse()
+                               .join('');
+
+      return oldString === newString;
 };
 
 // Do not edit below this line
