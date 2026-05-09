aa1b5790dae0ce8e65c1e5b94a134c7d092c9aca
Spxchaos <amannaiknov@gmail.com>
2026-05-09 17:31:28 +0200

    done

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+ 
+        return array.reduce((oldest, person) => {
+
+        const oldestAge = oldest.yearOfDeath
+            ? oldest.yearOfDeath - oldest.yearOfBirth
+            : new Date().getFullYear() - oldest.yearOfBirth;
+
+        const personAge = person.yearOfDeath
+            ? person.yearOfDeath - person.yearOfBirth
+            : new Date().getFullYear() - person.yearOfBirth;
+
+        return personAge > oldestAge ? person : oldest;
+
+    });
 
 };
 
